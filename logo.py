f6a4f6d7d2efcd6aee4872797dc00ba58c948da5
GabrielPacheco <gabioh2012@gmail.com>
2023-09-11 19:47:13 -0500

    Mejora en el logo

--- logo.py
+++ logo.py
@@ -5,30 +5,27 @@ def add_logo():
         """
         <style>
             [data-testid="stSidebarNav"] {
-                background-image: url(https://www.teleamazonas.com/wp-content/uploads/2023/09/identidadWEB.png);
-                background-repeat: no-repeat;
-                padding-top: 10px;
-                background-size: cover;
-                background-opacity: 0.5;
-                position: relative;
-            }
-            [data-testid="stSidebarNav"]::before {
-                content: " ";
-                position: absolute;
-                width: 100%;
-                height: 25%;                                     
-                top: 25%;
                 background-image: url(https://ister.edu.ec/wp-content/uploads/2021/01/RU-LOGO-COMPLETO.webp);
                 background-repeat: no-repeat;
-                padding-top: 15px;
-                background-size: 90%;
+                background-size: 80%;
+                background-position: 25% 15%;
+                position: relative;
+                
             }
-
-         
-                     
+            [data-testid="stSidebarNav"]::before {
+                position: absolute;
+                content: "  ";
+                width: 100%;
+                height: 100%;
+                background-image: url(https://www.teleamazonas.com/wp-content/uploads/2023/09/identidadWEB.png);
+                background-repeat: no-repeat;
+                background-size: cover;
+                z-index: -1;
+                opacity: 0.40;
+            }
         </style>
         """,
         unsafe_allow_html=True,
     )
-st.set_page_config( page_title= "Encontrar Programa", page_icon="Home")
+
 
